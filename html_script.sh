6f193bc246dc276073c21833771624069bd6e0aa
EC2 uploader <example@exmaple.com>
2020-12-28 23:24:56 +0000

    new version

--- html_script.sh
+++ html_script.sh
@@ -5,6 +5,7 @@ jupyter nbconvert --to notebook --execute generator.ipynb
 jupyter nbconvert --to html generator.nbconvert.ipynb --TagRemovePreprocessor.remove_cell_tags "remove_cell" --TagRemovePreprocessor.remove_input_tags "remove_input"
 rm generator.nbconvert.ipynb
 rm data.log
+mv generator.nbconvert.html index.html
 git add .
 git commit -m "new version"
 git push
